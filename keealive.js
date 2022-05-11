65ea0dbd77f38dfc03c26884431db3bf3b1fc615
ZeroX0001 <87260173+ZeroX0001@users.noreply.github.com>
2022-05-11 17:29:37 +0800

    Update keealive.js

--- keealive.js
+++ keealive.js
@@ -4,4 +4,4 @@ const port = 3000;
 
 app.get('/', (req, res) => res.send('Yo! Discord bot is not online 24/7\nMake sure to host it in our Discord server!'));
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
+app.listen(port, () => console.log(`RadioBot application is listening at ${port}`));
