9b5e40f5468be8c36dee1806910579916b4a2c32
ZeroX0001 <87260173+ZeroX0001@users.noreply.github.com>
2022-05-12 09:23:59 +0800

    Add files via upload

---
+++ keepalive.js.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => res.send('Yo! Discord bot is not online 24/7\nMake sure to host it in our Discord server!'));
+
+app.listen(port, () => console.log(`RadioBot application is listening at ${port}`));
